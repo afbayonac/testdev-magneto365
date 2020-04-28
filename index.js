e8058290c176dfe2497afed9c90094833ec38240
afbayonac <afbayonac@gmail.com>
2020-04-28 09:45:57 -0500

    Filter startships

--- index.js
+++ index.js
@@ -23,15 +23,33 @@ async function filmsSW () {
   const FILMS_URL = 'https://swapi.dev/api/films/'
   const response = await fetch(FILMS_URL)
   const movies = response.results
-  const species = await fetchUrls(flatByPrperty(movies, 'species'))
-  // const planets = await fetchUrls(flatByPrperty(movies, 'p'))
-  // const species = await fetchUrls(flatByPrperty(movies, 'species'))
-  // const species = await fetchUrls(flatByPrperty(movies, 'species'))
-  // const species = await fetchUrls(flatByPrperty(movies, 'species'))
+
+  const planets = await fetchUrls(flatByPrperty(movies, 'planets'))
+  const characters = await fetchUrls(flatByPrperty(movies, 'species'))
+  const species = await fetchUrls(flatByPrperty(characters, 'species'))
+  const startship = await fetchUrls(flatByPrperty(movies, 'startship'))
+
   console.log(species)
   return movies.map(movie => ({
-    ...movie,
-    species
+    name: movie.name,
+    planets: planets.map(planete => ({
+
+    })),
+    characters: planets.map(planete => ({
+
+    })),
+    startships: planets
+      .reduce((aco, startship) => {
+        if (!aco || aco.length < startship.length) {
+          return startship
+        } else {
+          return aco
+        }
+      })
+      .map(planete => ({
+
+      }))
+
   }))
 }
 
